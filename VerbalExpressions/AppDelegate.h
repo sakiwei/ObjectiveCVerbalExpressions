93083cbb0d849ca94c2431f5c3e4e23336534cce
sakiwei <sakiwei@gmail.com>
2013-08-06 22:30:11 +0800

    update copyrights export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/

--- VerbalExpressions/AppDelegate.h
+++ VerbalExpressions/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  VerbalExpressions
 //
-//  Created by Wai Cheung on 6/8/13.
-//  Copyright (c) 2013 Wai Cheung. All rights reserved.
+//  Created by sakiwei on 6/8/13.
+//  Copyright (c) 2013 sakiwei. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
